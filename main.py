74774fd1a6f48291822d62593f3c0280426da236
VojtechKrejcik <vojtech.krejcik@gmail.com>
2020-10-06 14:57:58 +0200

    sablona pro homepage udelana, chce to jeste nejake opravy

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ import re
 app = Flask(__name__)
 app.secret_key = 'prdel'
 
-# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
-@app.route('/pythonlogin/', methods=['GET', 'POST'])
+# http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', msg='')
+    if request.method == 'GET':
+        return render_template('login.html', msg='')
+
+    elif request.method == 'POST':
+        #potrbujeme implementovat login
+        return redirect(url_for('home'))
+
+@app.route('/home/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html',msg='')
